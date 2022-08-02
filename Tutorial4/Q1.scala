a0efe1d9f7dd3961e7b1fa4ab5e8473e0d2c391d
Muhammad Suraif <suraifcoc16@gmail.com>
2022-08-02 11:00:19 +0530

    Update Q1.scala

--- Tutorial4/Q1.scala
+++ Tutorial4/Q1.scala
@@ -3,8 +3,8 @@ package Tutorial4
 object Q1 extends App {
   // Calculating the interest
   def getInterest(deposit: Double): Double = deposit match{
-    case x if(x > 2000000) => x * 0.065
-    case x if(x > 200000) => x * 0.035
+    case x if(x > 2000000) => x * 0.035
+    case x if(x > 200000) => x * 0.065
     case x if(x > 20000) => x * 0.04
     case x => x * 0.002
   }
