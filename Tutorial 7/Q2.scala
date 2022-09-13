4b1f8c4345bec5df8229ef5eb91ba64f218740ec
Muhammad Suraif <suraifcoc16@gmail.com>
2022-09-13 09:35:24 +0530

    Update Q2.scala

--- Tutorial 7/Q2.scala
+++ Tutorial 7/Q2.scala
@@ -1,8 +1,25 @@
-object Q2 extends App{
-  //create objects
-  val r1 = new Rational(3,4)
-  val r2 = new Rational(5,8)
-  val r3 = new Rational(2,7)
-  println(" r1-r2-r3 : " + (r1-r2-r3))
+package scala_tutorial7
+
+object Q2 extends App {
+  val x = new RationalNumber(3,4)
+  val y = new RationalNumber(5,8)
+  val z = new RationalNumber(2,7)
+
+  val result = x.Sub(y).Sub(z)
+
+  println(result)
 }
+
+class RationalNumber(a: Int, b: Int) {
+  private def gcd (p: Int, q: Int): Int =
+    if (q == 0)  p else gcd(q, p % q)
+
+  def numer = a / gcd(a, b)
+  def denom = b / gcd(a, b)
+
+  def Sub (r:RationalNumber) = new RationalNumber(this.numer * r.denom - this.denom * r.numer, this.denom * r.denom)
+  override def toString(): String = numer + "/" + denom
+}
+
+
 
