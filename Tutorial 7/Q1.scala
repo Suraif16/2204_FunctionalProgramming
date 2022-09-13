817a769066ce7078efe4de4674d60c997252f19a
Muhammad Suraif <suraifcoc16@gmail.com>
2022-09-13 09:43:18 +0530

    Update Q1.scala

--- Tutorial 7/Q1.scala
+++ Tutorial 7/Q1.scala
@@ -1,23 +1,14 @@
-object Q1 extends App{
-  //create a variable of Rational data type
-  val r1 = new Rational(3,7)
-  println(r1)
-  //get negative
-  println("Negative value of the rational number : " + r1.neg)
+package scala_tutorial7
 
+object Q1 extends App {
+  val Num = new Rational(5,6)
+  println(Num.neg())
 }
 
-//Define Rational data type
-class Rational(n:Int, d:Int){
-  def numer = n
-  def denom = d
+class Rational(numerator:Int, denominator:Int){
+  def x:Int = numerator
+  def y:Int = denominator
 
-  //function for get negative Rational number
-  def neg = new Rational(-this.numer, this.denom)
-  //function for add two rational numbrt
-  def +(that:Rational) = new Rational(this.numer * that.denom + this.denom * that.numer, this.denom * that.denom)
-  //function for subtract
-  def -(that:Rational) = this + that.neg
-  //print the string
-  override def toString = numer + "/" + denom
+  def neg() = new Rational(-this.x, this.y)
+  override def toString: String = x + "/" + y
 }
